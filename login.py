c9d6b83e20f9c777820c2e510b16d521fbd19ece
Albin Varghese <albin8285@gmail.com>
2020-11-07 23:08:26 +0530

    fixed indentation issues

--- login.py
+++ login.py
@@ -6,15 +6,15 @@ print("create your account")
 name=input("give your name:")
 user=input ("give your unique username:")
 if us.islower():
-     password=input("choose your strong password")
-     if len(password) < 8:
-         print("Make sure your password is at lest 8 letters")
-    elif not password.isdigit():
-        print("Make sure your password has a number")
-    elif not password.isupper():
-        print("Make sure your password has a capital letter")
-    else:
-        print("Your username and password inputted successfully")
+	password=input("choose your strong password")
+	if len(password) < 8:
+		print("Make sure your password is at lest 8 letters")
+	elif not password.isdigit():
+		print("Make sure your password has a number")
+	elif not password.isupper():
+		print("Make sure your password has a capital letter")
+	else:
+		print("Your username and password inputted successfully")
 else:
     print("username invalid!")
 salt = os.urandom(32)
@@ -26,9 +26,9 @@ pa= input("password : ")
 salt = os.urandom(32)
 pa= hashlib.pbkdf2_hmac( 'sha256', password.encode('utf-8'), salt,  100000 )
 if username == us :
-    if password == pa:
-        print("Conngratulations!Login successfully finished.")
-    else :
-        print("invalid password")
+	if password == pa:
+		print("Conngratulations!Login successfully finished.")
+	else :
+		print("invalid password")
 else :
-    print("Invalid username")
+	print("Invalid username")
