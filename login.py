844cbaeb3de874a89cdd418362e0f7444b67c877
Albin Varghese <albin8285@gmail.com>
2020-11-07 23:14:49 +0530

    add line breaks and improved messages

--- login.py
+++ login.py
@@ -1,12 +1,18 @@
 import hashlib
 import os
-print("hello")
+
+salt = os.urandom(32)
+
 print("Hello all,this is my simple program of login system using python")
+print("\n")
 print("create your account")
-name=input("give your name:")
-user=input ("give your unique username:")
+print("\n")
+
+name=input("Username : ")
+user=input ("Password : ")
+
 if us.islower():
-	password=input("choose your strong password")
+	password=input("Choose a strong Password")
 	if len(password) < 8:
 		print("Make sure your password is at lest 8 letters")
 	elif not password.isdigit():
@@ -14,13 +20,16 @@ if us.islower():
 	elif not password.isupper():
 		print("Make sure your password has a capital letter")
 	else:
-		print("Your username and password inputted successfully")
+		print("Your username and password generated successfully")
 else:
-    print("username invalid!")
-salt = os.urandom(32)
+    print("Username Invalid!")
+
 password= hashlib.pbkdf2_hmac( 'sha256', password.encode('utf-8'), salt,  100000 )
+
 print("Account created successfully")
+print("\n")
 print ("Login to your account")
+print("\n")
 us= input("Username : ")
 pa= input("password : ")
 salt = os.urandom(32)
