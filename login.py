75380994769c53ff687dbb6faf1bdff3aafed179
Albin Varghese <albin8285@gmail.com>
2020-11-07 23:40:14 +0530

    add comments

--- login.py
+++ login.py
@@ -1,8 +1,10 @@
 import hashlib
 import os
 
+# Generate salt
 salt = os.urandom(32)
 
+#Registration Section
 print("\n")
 print("Hello all,this is my simple program of login system using python")
 print("Create your account")
@@ -10,6 +12,7 @@ name=input("Username : ")
 username=input ("Password : ")
 print("\n")
 
+# Validate Data
 if username.islower():
 	password=input("Choose a strong Password : ")
 	if len(password) < 8:
@@ -23,16 +26,21 @@ if username.islower():
 else:
     print("invalid username!")
 
-passwd= hashlib.pbkdf2_hmac( 'sha256', password.encode('utf-8'), salt,  100000 )
+#Encrypt Password
+passwd = hashlib.pbkdf2_hmac( 'sha256', password.encode('utf-8'), salt,  100000 )
 
 print("Account created successfully")
 print("\n")
+
+# Login Module
 print ("Login to your account")
 print("\n")
 us= input("Username : ")
-pa= input("password : ")
+pa= input("Password : ")
 pa= hashlib.pbkdf2_hmac( 'sha256', password.encode('utf-8'), salt,  100000 )
 print("\n")
+
+# User Authentication
 if username == us :
 	if passwd == pa:
 		print("Conngratulations! Login successful.")
