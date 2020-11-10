00e423c3cd6073025c507bf87078cbae45c38ca9
Jayashakar J <jayashakar350@gmail.com>
2020-11-10 12:42:35 +0530

    Your new Upgrade

--- login.py
+++ login.py
@@ -13,18 +13,28 @@ username=input ("Password : ")
 print("\n")
 
 # Validate Data
+j=1
+while j==3:
 if username.islower():
+	j=3
+	i=1
+	while i==2:
 	password=input("Choose a strong Password : ")
 	if len(password) < 8:
 		print("Make sure your password is at lest 8 letters")
+		i=1
 	elif not password.isdigit():
 		print("Make sure your password has a number")
+		i=1
 	elif not password.isupper():
 		print("Make sure your password has a capital letter")
+		i=1
 	else:
 		print("Your username and password generated successfully")
+		i=2
 else:
     print("invalid username!")
+    j=1
 
 #Encrypt Password
 passwd = hashlib.pbkdf2_hmac( 'sha256', password.encode('utf-8'), salt,  100000 )
